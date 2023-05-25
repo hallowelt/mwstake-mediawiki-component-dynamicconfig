4d9653aa8e736f9be642f4efd0b13f1b0d2d2a36
d.savuljesku <d.savuljesku@gmail.com>
2023-05-25 09:35:33 +0200

    Use Globals when registering hook

--- bootstrap.php
+++ bootstrap.php
@@ -7,22 +7,17 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_DYNAMICCONFIG_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_DYNAMICCONFIG_VERSION', '1.0.0' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_DYNAMICCONFIG_VERSION', '1.0.1' );
 
 Bootstrapper::getInstance()
 	->register( 'dynamicconfig', function () {
 		$GLOBALS['wgServiceWiringFiles'][] = __DIR__ . '/includes/ServiceWiring.php';
 		$GLOBALS['wgMWStakeDynamicConfigs'] = [];
 
-		$GLOBALS['wgExtensionFunctions'][] = static function () {
-			$hookContainer = MediaWikiServices::getInstance()->getHookContainer();
-			$hookContainer->register(
-				'LoadExtensionSchemaUpdates',
-				static function ( DatabaseUpdater $updater ) {
-					$updater->addExtensionTable(
-						'mwstake_dynamic_config', __DIR__ . '/db/mwstake_dynamic_config.sql'
-					);
-				} );
+		$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'] = static function ( DatabaseUpdater $updater ) {
+			$updater->addExtensionTable(
+				'mwstake_dynamic_config', __DIR__ . '/db/mwstake_dynamic_config.sql'
+			);
 		};
 
 		$GLOBALS['wgHooks']['SetupAfterCache'] = $GLOBALS['wgHooks']['SetupAfterCache'] ?? [];
