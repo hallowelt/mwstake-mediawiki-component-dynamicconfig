b95dfcb31ceaefd90276bb6bf8ec7b13094f2b7a
d.savuljesku <d.savuljesku@gmail.com>
2025-01-14 13:56:55 +0100

    Remove deprecated access to

--- bootstrap.php
+++ bootstrap.php
@@ -7,29 +7,28 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_DYNAMICCONFIG_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_DYNAMICCONFIG_VERSION', '1.0.9' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_DYNAMICCONFIG_VERSION', '1.0.10' );
 
 Bootstrapper::getInstance()
 	->register( 'dynamicconfig', static function () {
 		$GLOBALS['wgServiceWiringFiles'][] = __DIR__ . '/includes/ServiceWiring.php';
 		$GLOBALS['wgMWStakeDynamicConfigs'] = [];
 
-		$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'][] = static function ( DatabaseUpdater $updater ) {
-			$dbType = $updater->getDB()->getType();
-			$updater->addExtensionTable(
-				'mwstake_dynamic_config', __DIR__ . "/db/$dbType/mwstake_dynamic_config.sql"
-			);
-			$updater->modifyExtensionField(
-				'mwstake_dynamic_config',
-				'mwdc_serialized',
-				__DIR__ . "/db/$dbType/mwstake_dynamic_config_serialized_patch.sql"
-			);
+		$GLOBALS['wgExtensionFunctions'][] = static function() {
+			$hookContainer = MediaWikiServices::getInstance()->getHookContainer();
+			$hookContainer->register( 'LoadExtensionSchemaUpdates', static function ( DatabaseUpdater $updater ) {
+				$updater->addExtensionTable(
+					'mwstake_dynamic_config', __DIR__ . '/db/mwstake_dynamic_config.sql'
+				);
+				$updater->modifyExtensionField(
+					'mwstake_dynamic_config',
+					'mwdc_serialized',
+					__DIR__ . '/db/mwstake_dynamic_config_serialized_patch.sql'
+				);
+			} );
+			$hookContainer->register( 'SetupAfterCache', static function () {
+				$manager = MediaWikiServices::getInstance()->getService( 'MWStakeDynamicConfigManager' );
+				$manager->loadConfigs();
+			} );
 		};
-
-		$GLOBALS['wgHooks']['SetupAfterCache'] = $GLOBALS['wgHooks']['SetupAfterCache'] ?? [];
-		array_unshift( $GLOBALS['wgHooks']['SetupAfterCache'], static function () {
-			// Earliest point we can access services I found
-			$manager = MediaWikiServices::getInstance()->getService( 'MWStakeDynamicConfigManager' );
-			$manager->loadConfigs();
-		} );
 	} );
